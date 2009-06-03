d27468c4196531c827948e056e92c98278c9619a
tony.benbrahim <tony.benbrahim@b0caed8a-5031-11de-b1b8-3717d94492c0>
2009-06-03 11:49:08 +0000

    initial parser/lexer and AST printer

---
+++ jtemplate/ast_info.ml
@@ -0,0 +1,113 @@
+module AstInfo=struct
+
+let prefix level=
+	match level with
+	  0 -> ""
+	| _ -> (String.make ((level-1)*3) ' ')^"+--"
+
+let opname=function
+	  Ast.Plus -> "+"
+	| Ast.Minus -> "-"
+	| Ast.Divide -> "/"
+	| Ast.Times -> "*"
+	| Ast.Modulo -> "%"
+
+let compopname=function
+	  Ast.LessThan -> "<"
+	| Ast.LessThanEqual -> ">"
+	| Ast.Equal -> "=="
+	| Ast.GreaterThan -> ">"
+	| Ast.GreaterThanEqual -> ">="
+	| Ast.NotEqual -> "!="
+
+let rec statement_descriptionl level statement =
+	match statement with 
+	Ast.StatementBlock(stmts) -> 
+		(prefix level)^ "Statement Block\n" ^ 
+			(statement_list (level+1) stmts) 
+	|	Ast.Assignment(var,expr) -> (prefix level)^"Assignment\n" ^
+	    (var_descriptionl (level+1) var) ^ (expr_descriptionl (level+1) expr)
+	| Ast.Declaration(var,expr) -> (prefix level)^"Declaration\n" ^
+	    (var_descriptionl (level+1) var) ^ (expr_descriptionl (level+1) expr)
+	| Ast.Instructions(var,varlist,specs) -> (prefix level)^"Instructions\n" ^
+		(var_descriptionl (level+1) var) ^
+		(List.fold_left (fun acc el -> (var_descriptionl (level+1) el)^acc) "" varlist) ^
+		(List.fold_left (fun acc el -> let (Ast.Label(label),condspec,repllist)=el in
+		   acc ^ (prefix (level+1)) ^ "Label "  ^ label ^ "\n" ^ 
+	 			(match condspec with
+				  Ast.Once -> (prefix (level+2)) ^ "once\n"
+				| Ast.When(expr) -> (prefix (level+2)) ^ "when\n" ^ (expr_descriptionl (level+3) expr)
+				| Ast.Loop(var,expr) -> (prefix (level+2)) ^"foreach\n" ^ 
+				    (var_descriptionl (level+3) var) ^ (expr_descriptionl (level+3) expr)
+				) ^ (List.fold_left (fun acc el-> let (text,expr)=el in 
+				     acc^(prefix (level+3))^text^"=\n"^(expr_descriptionl (level+4) expr)) "" repllist )
+			) "" specs)
+	| Ast.TemplateDef(var,specs) -> (prefix level)^"Template Definition\n" ^
+		(var_descriptionl (level+1) var) ^ 
+		(List.fold_left (fun acc el -> let (optlabel,text)=el in 
+			match optlabel with 
+				Some Ast.Label(label) -> acc ^label ^":\t"^text^"\n"
+			|	None -> acc ^":\t"^text^"\n"
+				) "" specs)
+	| Ast.If(expr,iflist,elselist) -> (prefix level)^"If/Else\n"^
+	  (expr_descriptionl (level+1) expr)^
+		(statement_descriptionl (level+1) iflist)^
+		(statement_descriptionl (level+1) elselist)
+	| Ast.Return(expr) -> (prefix level)^"Return\n"^(expr_descriptionl (level+1) expr)
+	| Ast.ExpressionStatement(expr) -> expr_descriptionl level expr
+	| Ast.While(expr,stmt) -> (prefix level)^"While\n" ^
+	  (expr_descriptionl (level+1) expr) ^
+		(statement_descriptionl (level+1) stmt)
+	| Ast.ForEach(var,expr,stmt) -> (prefix level)^"ForEach\n" ^
+	    (var_descriptionl (level+1) var) ^ (expr_descriptionl (level+1) expr) ^
+			(statement_descriptionl (level+1) stmt)
+	| Ast.Continue -> (prefix level)^"Continue\n"
+	| Ast.Break -> (prefix level)^"Break\n"
+and 
+var_descriptionl level var = 
+	match var with
+	  Ast.Name(name) -> (prefix level) ^ "Variable " ^ name ^ "\n"
+	| Ast.CompoundName(name_list) -> (prefix level) ^ "MapReference " ^ (List.fold_left (fun acc el -> el ^ "." ^ acc) "" name_list) ^ "\n" 
+and 
+expr_descriptionl level expr=
+	match expr with
+	  Ast.BinaryOp(op1, op, op2) -> (prefix level) ^ "BinOp "^ (opname op) ^ "\n" ^
+		  (expr_descriptionl (level+1) op1) ^ (expr_descriptionl (level+1) op2)
+	| Ast.CompOp(op1, op, op2) -> (prefix level) ^ "CompOp " ^ (compopname op) ^ "\n" ^
+		  (expr_descriptionl (level+1) op1) ^ (expr_descriptionl (level+1) op2)
+	| Ast.Value(value) -> value_descriptionl level value
+	| Ast.FunctionCall(var,expr_list) -> (prefix level) ^"FunctionCall "^ (var_descriptionl 0 var) ^
+		(expression_list (level+1) expr_list)
+and
+value_descriptionl level value=
+	match value with
+	  Ast.Integer(v) -> (prefix level) ^ "Integer "^(string_of_int v)^"\n"
+	| Ast.Variable(v) -> var_descriptionl level v
+	| Ast.Map(v) -> (prefix level) ^ "Map\n" ^ (property_list (level+1) v)^"\n"
+	| Ast.Array(v) -> (prefix level) ^ "Array\n" ^ (expression_list (level+1) v)
+	| Ast.Function(arglist,stmts) -> (prefix level)^"Function\n" ^
+		(List.fold_left (fun acc el -> (var_descriptionl (level+1) el)^acc) "" arglist) ^
+		(statement_list (level+1) stmts) 
+	| Ast.Boolean(v) -> (prefix level) ^ "Boolean " ^ (string_of_bool v) ^ "\n" 
+	| Ast.String(v) -> (prefix level) ^ "String " ^ v ^ "\n"
+	| Ast.Float(v) -> (prefix level) ^ "Float " ^ (string_of_float v) ^ "\n"
+	| Ast.Expression(expr) -> expr_descriptionl level expr
+and
+statement_list level stmt_list= 
+	List.fold_left (fun acc el -> acc^(statement_descriptionl level el)) "" stmt_list
+and
+expression_list level expr_list=
+	List.fold_left (fun acc el -> acc^(expr_descriptionl level el)) "" expr_list
+and
+property_list level prop_list= 
+	List.fold_left (fun acc el -> let (name,expr)=el in 
+		acc^(prefix level)^"Property "^name^"\n"^(expr_descriptionl (level+1) expr)) "" prop_list
+	
+let statement_description=statement_descriptionl 0
+let var_description=var_descriptionl 0
+let expr_description=expr_descriptionl 0	
+
+let print_ast statement=
+	print_string (statement_description statement) 
+		
+end
---
+++ jtemplate/lexer.mll
@@ -0,0 +1,96 @@
+{
+open Ast
+open Parser
+
+(* from http://plus.kaist.ac.kr/~shoh/ocaml/ocamllex-ocamlyacc/ocamllex-tutorial.pdf , p.9 *)
+let incr_linenum lexbuf =
+	let pos = lexbuf.Lexing.lex_curr_p in
+	lexbuf.Lexing.lex_curr_p <- { pos with
+		Lexing.pos_lnum = pos.Lexing.pos_lnum + 1;
+		Lexing.pos_bol = pos.Lexing.pos_cnum;
+		}
+
+exception LexerException of string * int * int
+exception Eof
+
+let syntax_exception msg lexbuf=
+	raise (LexerException (msg, lexbuf.Lexing.lex_curr_p.Lexing.pos_lnum,
+					   lexbuf.Lexing.lex_curr_p.Lexing.pos_cnum - lexbuf.Lexing.lex_curr_p.Lexing.pos_bol))
+
+let map_id name=
+	match name with
+		"foreach" -> FOREACH
+	| "in" -> IN
+	|	"while" -> WHILE
+	| "function" -> FUNCTION
+	|	"if" -> IF
+	| "else" -> ELSE
+	|	"template" -> TEMPLATE
+	|	"instructions" ->INSTRUCTIONS
+	| "continue" -> CONTINUE
+	| "break" -> BREAK
+	| "return" -> RETURN
+	| "for" -> FOR
+	| "once" -> ONCE
+	| "when" -> WHEN
+	| "var" -> VAR
+	| "true" -> BOOLEAN(true)
+	| "false" -> BOOLEAN(false)
+	|	_ ->  ID(name)
+		
+}
+		
+let digit = ['0'-'9']
+let id = ['a'-'z' 'A'-'Z' '_' '$']['A'-'Z' 'a'-'z' '0'-'9' '_' '$' ]*
+let whitespace = ['\r' '\t' ' ']
+let text = '#'[^'\n']*
+
+rule main =	parse 
+| whitespace { main lexbuf }
+| text as token { TEXT(String.sub token 1 ((String.length token) - 1))}
+| digit+ as token { INT( int_of_string token)}
+| digit+ '.' digit* | digit* '.' digit+ as token { REAL(float_of_string token)}
+| id as token { (map_id token )}
+| '\'' { single_quote_string "" lexbuf } 
+| "//" [^'\n']* { main lexbuf}
+| "/*" {multiline_comment lexbuf}
+| '\n' { incr_linenum lexbuf;main lexbuf }
+| "&&" {AND}
+| "||" {OR}
+| "<" {COMPOP(LessThan)}
+| ">" {COMPOP(GreaterThan)}
+| "<=" {COMPOP(LessThanEqual)}
+| ">=" {COMPOP(GreaterThanEqual)}
+| "==" {COMPOP(Equal)}
+| "!=" {COMPOP(NotEqual)}
+| '=' {EQUALS} 
+| '.' {DOT}
+| '{' {LBRACE}
+| '}' {RBRACE}
+| '(' {LPAREN}
+| ')' {RPAREN}
+| '[' {LBRACKET}
+| ']' {RBRACKET}
+| ',' {COMMA} 
+| ';' {SEMICOLON}
+| ':' {COLON}
+| '!' {NOT}
+| '?' {QUESTION}
+| '+' {PLUS}
+| '-' {MINUS}
+| '*' {TIMES}
+| '/' {DIVIDE}
+| '%' {MODULO}
+| _ as c { syntax_exception ("Invalid character "^String.make 1 c) lexbuf} 
+| eof { EOF } 
+and single_quote_string s = parse
+| '\n'  { incr_linenum lexbuf; single_quote_string (s ^ "\n") lexbuf }
+| "\\'"  { single_quote_string (s ^ "\'") lexbuf }
+| "'"  { STRING(s) }
+| [^ '\''] as c  { single_quote_string (s ^ String.make 1 c) lexbuf }
+| eof  { syntax_exception "Unterminated string constant" lexbuf }
+and multiline_comment = parse
+| '\n' { incr_linenum lexbuf; multiline_comment lexbuf }
+| "*/" { main lexbuf }
+| [^ '\n'] { multiline_comment lexbuf}
+| eof { syntax_exception "Unterminated multiline comment" lexbuf } 
