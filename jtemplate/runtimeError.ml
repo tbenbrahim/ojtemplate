ac89147b1d827dbbd77f2e351d9d815674200705
tony.benbrahim <tony.benbrahim@b0caed8a-5031-11de-b1b8-3717d94492c0>
2009-06-13 20:07:16 +0000

    implementation and tests of varargs functionality

--- jtemplate/runtimeError.ml
+++ jtemplate/runtimeError.ml
@@ -42,10 +42,15 @@ exception TypeMismatchInMapAssignment of string * string * string * string
 (** This exception indicates that the number of arguments passed in
 to a function call do not match with number of arguments in the formal arguments
 @param field1 the name of the function
-@param field2 the number of formal arguments
-@param field3 the number of arguments passed in during the call
 *)
-exception MismatchedCallArgs of string * int * int
+exception MismatchedCallArgs of string
+
+exception TypeMismatchInMapAssignment of string * string * string * string
+
+(** This exception indicates that a vararg is not last in function definition
+@param field1 the name of the function
+*)
+exception VarArgsMustbeLast of string
 
 (** Generic library call error *)
 exception LibraryError of string
@@ -66,9 +71,10 @@ let string_of_error ex =
 			"Cannot assign a "^new_type^" to variable "^name^" of type "^old_type
 	| TypeMismatchInMapAssignment(comp, name, old_type, new_type) ->
 			"Cannot assign a "^new_type^" to in "^comp^" of type "^old_type^" in map "^name
-	| MismatchedCallArgs(name, num_formal, num_call) ->
-			"In function call to " ^ name ^ ", expected " ^ (string_of_int num_formal) ^
-			" arguments, got "^ (string_of_int num_call) ^ " instead."
+	| MismatchedCallArgs(name) ->
+			"In function call to " ^ name ^ ", the number of parameters do not match the number of formal arguments"
+	| VarArgsMustbeLast(name) ->
+			"In definition of "^name^", a vararg parameter must be the last formal parameter"
 	| LibraryError msg -> msg
 	| InvalidArrayIndex(ind, name) -> "Invalid array index "^ind^" in "^name
 	| e -> "uncaught exception "^(Printexc.to_string e)
