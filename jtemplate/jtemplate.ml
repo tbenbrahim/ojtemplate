28412421831692f9c649fe4996c055813cf486e9
tony.benbrahim <tony.benbrahim@b0caed8a-5031-11de-b1b8-3717d94492c0>
2009-06-14 09:49:53 +0000

    read script filename from command line

--- jtemplate/jtemplate.ml
+++ jtemplate/jtemplate.ml
@@ -11,16 +11,35 @@ open Library_helper
 open Ast
 open Filename_util
 
+let rec register_args ind len symbol_table =
+	if ind = 1 then
+		SymbolTable.declare (Name("args")) (MapValue(Hashtbl.create (len - 1), ArraySubtype)) symbol_table
+	else ();
+	if ind = len then
+		SymbolTable.declare (CompoundName(["args";"length"])) (IntegerValue(len - 1)) symbol_table
+	else
+		(
+			SymbolTable.declare (CompoundName(["args"; string_of_int (ind - 1)])) (StringValue(Sys.argv.(ind))) symbol_table;
+			register_args (ind + 1) len symbol_table
+		)
+
 let _ =
 	let symbol_table = SymbolTable.initialize_environment
 			{ parse_callback = (Parser_util.parse_filename);
-				loaded_imports =[] ;current_stmt=("",0);
-				stack_trace= []; } in
+				loaded_imports =[] ; current_stmt = ("", 0);
+				stack_trace = []; } in
 	register_library BuiltinLibrary.exported symbol_table;
 	register_library StringLibrary.exported symbol_table;
 	register_library IOLibrary.exported symbol_table;
 	let _ = Parsing.set_trace false in
-	let ast = Parser_util.parse_filename
-			(resolve_filename (Unix.getcwd()) "..\\samples\\import_demo.jtp")
-	in
-	Interpreter.interpret_statement ast symbol_table
+	let argl = Array.length Sys.argv in
+	if argl < 2 then
+		prerr_string ("Usage: "^(Filename.basename Sys.argv.(0))^" scriptfile [args...]\n")
+	else
+		(
+			register_args 1 argl symbol_table;
+			let filename = Sys.argv.(1) in
+			let ast = if filename ="-" then Parser_util.parse stdin "stdin" else Parser_util.parse_filename (resolve_filename (Unix.getcwd()) filename)
+			in
+			Interpreter.interpret_statement ast symbol_table
+		)
